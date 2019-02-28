bb636132d22c2357eb5ee32360b1241e9d0bae62
T. J. Alferes <tjalferes@gmail.com>
2019-02-28 15:50:28 -0600

    mouse aim fully working

--- src/containers/HeaderRed/SiteNav/Menu/Menu.js
+++ src/containers/HeaderRed/SiteNav/Menu/Menu.js
@@ -1,9 +1,11 @@
 import React, { useState, useLayoutEffect } from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 
 import './menu.css';
 import NutritionSlideImage from '../../../../assets/images/header/dropdowns/steve-reeves-nutrition-slide.png';
+import MethodsSlideImage from '../../../../assets/images/header/dropdowns/fire-methods-slide.png';
+import IngredientsSlideImage from '../../../../assets/images/header/dropdowns/abundance-ingredients-slide.png';
 import CuisinesSlideImage from '../../../../assets/images/header/dropdowns/world-map-cuisines-slide.png';
 
 /*
@@ -14,12 +16,8 @@ https://github.com/jasonslyvia/react-menu-aim
 https://github.com/kamens/jQuery-menu-aim
 */
 const MOUSE_LOCS_TRACKED = 3;   // number of past mouse locations to track
-const DELAY = 300;              // ms delay when user appears to be entering submenu
-const TOLERANCE = 75;           // bigger = more forgivey when entering submenu
-
-
-
-// DOM helpers
+const DELAY = 200;              // ms delay when user appears to be entering submenu
+const TOLERANCE = 50;           // bigger = more forgivey when entering submenu
 
 function offset(el) {
   if (!el) return {left: 0, top: 0};
@@ -44,10 +42,8 @@ function outerHeight(el) {
   return _height;
 }
 
-
-
 const Menu = props => {
-  const [activeMenuIndex, setActiveMenuIndex] = useState();  // 0?
+  const [activeMenuIndex, setActiveMenuIndex] = useState();
   //useFancyMenu ?
   //const getDefaultProps = () => ({submenuDirection: 'right'});
   //const getInitialState = () => ({activeMenuIndex: 0});
@@ -55,12 +51,9 @@ const Menu = props => {
   let __mouseMoveDocumentHandler;
   let __reactMenuAimTimer;
   let _lastDelayLoc;
-
-
   let mouseLocs = [];
 
-  // Mousemove handler on document
-  function handleMouseMoveDocument(e) {
+  function handleMouseMoveDocument(e) {  // Mousemove handler on document
     mouseLocs.push({x: e.pageX, y: e.pageY});
     if (mouseLocs.length > MOUSE_LOCS_TRACKED) mouseLocs.shift();
   }
@@ -113,13 +106,10 @@ const Menu = props => {
       __reactMenuAimTimer = setTimeout(() => {
         possiblyActivate.call(this, rowIdentifier, handler, config);
       }, delay);
-      console.log(__reactMenuAimTimer);
     } else {
       activate.call(this, rowIdentifier, handler);
     }
   }
-
-
 
   function initMenuAim(options) {
     __reactMenuAimConfig = options;
@@ -138,15 +128,9 @@ const Menu = props => {
   }
   
   const handleMouseEnterRow = (rowIdentifier, handler) => {
-    console.log('called');
-    console.log(__reactMenuAimTimer);
     if (__reactMenuAimTimer) clearTimeout(__reactMenuAimTimer);
-    console.log(__reactMenuAimTimer);
-    //console.log(this); // undefined
     //possiblyActivate.call(this, rowIdentifier, handler, __reactMenuAimConfig);
-    console.log(__reactMenuAimConfig);
     possiblyActivate(rowIdentifier, handler, __reactMenuAimConfig);
-    console.log(__reactMenuAimConfig);
   }
 
   useLayoutEffect(() => {  // useEffect() ???
@@ -176,7 +160,6 @@ const Menu = props => {
 
   const handleSwitchMenuIndex = index => {
     setActiveMenuIndex(index);
-    // ???
   }
 
   let containerClassName = 'menu-container ' + props.submenuDirection;
@@ -187,7 +170,7 @@ const Menu = props => {
         <ul>
           {props.menuData.map((menu, index) => {
             let className = 'menu-item';
-            if (activeMenuIndex && index === activeMenuIndex) className += ' active';
+            if (activeMenuIndex !== undefined && index === activeMenuIndex) className += ' active';
             return (
               <li
                 className={className}
@@ -201,16 +184,16 @@ const Menu = props => {
         </ul>
       </div>
       <div className="sub-menu">
-        <h3>{activeMenuIndex && props.menuData[activeMenuIndex].name}</h3>
+        <h3>{activeMenuIndex !== undefined && props.menuData[activeMenuIndex].name}</h3>
         <ul>
-          {activeMenuIndex && props.menuData[activeMenuIndex].subMenu.map((subMenu, index) => 
+          {activeMenuIndex !== undefined && props.menuData[activeMenuIndex].subMenu.map((subMenu, index) => 
             <li className="sub-menu-item" key={index}>{subMenu}</li>
           )}
         </ul>
-        {(activeMenuIndex && props.menuData[activeMenuIndex].image === 'nutrition') && <img src={NutritionSlideImage} />}
-        {(activeMenuIndex && props.menuData[activeMenuIndex].image === 'methods') && <img src={CuisinesSlideImage} />}
-        {(activeMenuIndex && props.menuData[activeMenuIndex].image === 'ingredients') && <img src={NutritionSlideImage} />}
-        {(activeMenuIndex && props.menuData[activeMenuIndex].image === 'cuisines') && <img src={CuisinesSlideImage} />}
+        {(activeMenuIndex !== undefined && props.menuData[activeMenuIndex].image === 'nutrition') && <img src={NutritionSlideImage} />}
+        {(activeMenuIndex !== undefined && props.menuData[activeMenuIndex].image === 'methods') && <img src={MethodsSlideImage} />}
+        {(activeMenuIndex !== undefined && props.menuData[activeMenuIndex].image === 'ingredients') && <img src={IngredientsSlideImage} />}
+        {(activeMenuIndex !== undefined && props.menuData[activeMenuIndex].image === 'cuisines') && <img src={CuisinesSlideImage} />}
       </div>
     </div>
   );
