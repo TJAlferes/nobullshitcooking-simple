179a9fbbcc91b005e18a5a808b533d569eb04f43
T. J. Alferes <tjalferes@gmail.com>
2019-07-17 11:37:37 -0500

    moving last images to s3

--- src/components/pages/Food/Methods/Methods.js
+++ src/components/pages/Food/Methods/Methods.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './methods.css';
-import Grill from '../../../../assets/images/content/grill.png';
+
+const ComingSoon = "https://nobsc-images-01.s3.amazonaws.com/content/misc/coming-soon-120-120.png";
 
 const Methods = props => (
   <div className={`methods one-column-a ${props.oneColumnATheme}`}>
@@ -11,37 +12,37 @@ const Methods = props => (
       <div className="nav-grid-a-item">
         <Link to="/food/methods/poach-and-simmer">
           <span className="nav-grid-a-item-text">Poach and Simmer</span>
-          <img className="nav-grid-a-item-image" src={Grill} />
+          <img className="nav-grid-a-item-image" src={ComingSoon} />
         </Link>
       </div>
       <div className="nav-grid-a-item">
         <Link to="/food/methods/steam">
           <span className="nav-grid-a-item-text">Steam</span>
-          <img className="nav-grid-a-item-image" src={Grill} />
+          <img className="nav-grid-a-item-image" src={ComingSoon} />
         </Link>
       </div>
       <div className="nav-grid-a-item">
         <Link to="/food/methods/saute">
           <span className="nav-grid-a-item-text">Saute</span>
-          <img className="nav-grid-a-item-image" src={Grill} />
+          <img className="nav-grid-a-item-image" src={ComingSoon} />
         </Link>
       </div>
       <div className="nav-grid-a-item">
         <Link to="/food/methods/roast">
           <span className="nav-grid-a-item-text">Roast</span>
-          <img className="nav-grid-a-item-image" src={Grill} />
+          <img className="nav-grid-a-item-image" src={ComingSoon} />
         </Link>
       </div>
       <div className="nav-grid-a-item">
         <Link to="/food/methods/grill">
           <span className="nav-grid-a-item-text">Grill</span>
-          <img className="nav-grid-a-item-image" src={Grill} />
+          <img className="nav-grid-a-item-image" src={ComingSoon} />
         </Link>
       </div>
       <div className="nav-grid-a-item">
         <Link to="/food/methods/stew-and-braise">
           <span className="nav-grid-a-item-text">Stew and Braise</span>
-          <img className="nav-grid-a-item-image" src={Grill} />
+          <img className="nav-grid-a-item-image" src={ComingSoon} />
         </Link>
       </div>
     </div>
--- src/components/pages/Food/Nutrition/Nutrition.js
+++ src/components/pages/Food/Nutrition/Nutrition.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './nutrition.css';
-import CaloriesThumb from '../../../../assets/images/content/calories-120-120.png';
-//import MacronutrientsThumb from '../../../../assets/images/content/macronutrients-120-120.png';
-import MicronutrientsThumb from '../../../../assets/images/content/micronutrients-120-120.png';
-import SupplementsThumb from '../../../../assets/images/content/supplements-120-120.png';
+
+const s3Path = "https://nobsc-images-01.s3.amazonaws.com/content/food/nutrition/";
 
 const Nutrition = props => (
   <div className={`nutrition one-column-a ${props.oneColumnATheme}`}>
@@ -14,25 +12,25 @@ const Nutrition = props => (
       <div className="nav-grid-a-item">
         <Link to="/food/nutrition/calories">
           <span className="nav-grid-a-item-text">Calories</span>
-          <img className="nav-grid-a-item-image" src={CaloriesThumb} />
+          <img className="nav-grid-a-item-image" src={`${s3Path}calories-120-120.png`} />
         </Link>
       </div>
       <div className="nav-grid-a-item">
         <Link to="/food/nutrition/macronutrients">
           <span className="nav-grid-a-item-text">Macronutrients</span>
-          <img className="nav-grid-a-item-image" src={MicronutrientsThumb} />
+          <img className="nav-grid-a-item-image" src={`${s3Path}macronutrients-120-120.png`} />
         </Link>
       </div>
       <div className="nav-grid-a-item">
         <Link to="/food/nutrition/micronutrients">
           <span className="nav-grid-a-item-text">Micronutrients</span>
-          <img className="nav-grid-a-item-image" src={MicronutrientsThumb} />
+          <img className="nav-grid-a-item-image" src={`${s3Path}micronutrients-120-120.png`} />
         </Link>
       </div>
       <div className="nav-grid-a-item">
         <Link to="/food/nutrition/supplements">
           <span className="nav-grid-a-item-text">Supplements</span>
-          <img className="nav-grid-a-item-image" src={SupplementsThumb} />
+          <img className="nav-grid-a-item-image" src={`${s3Path}supplements-120-120.png`} />
         </Link>
       </div>
     </div>
