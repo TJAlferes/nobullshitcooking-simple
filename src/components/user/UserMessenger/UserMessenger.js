233fbb96d90bdf7c6927c5de84a21f02ac185b3f
T. J. Alferes <tjalferes@gmail.com>
2019-08-31 02:04:20 -0500

    messenger css final

--- src/components/user/UserMessenger/UserMessenger.js
+++ src/components/user/UserMessenger/UserMessenger.js
@@ -11,11 +11,22 @@ import LeftNav from '../../LeftNav/LeftNav';
 import './userMessenger.css';
 
 const UserMessenger = props => {
+  const [ feedback, setFeedback ] = useState("");
+  const [ loading, setLoading ] = useState(false);
   //console.log(props);
   //const messagesRef = useRef(null);  //
   const messagesRef = createRef();
   //useImperativeHandle(ref, () => ({getNode: () => messagesRef.current}));
   const [ tab, setTab ] = useState("Room");
+
+  useEffect(() => {
+    let isSubscribed = true;
+    if (isSubscribed) {
+      if (props.feedback !== "") window.scrollTo(0,0);
+      setFeedback(props.feedback);
+    }
+    return () => isSubscribed = false;
+  }, [props.feedback]);
 
   useEffect(() => {
     const autoScroll = () => {
@@ -59,106 +70,113 @@ const UserMessenger = props => {
   return (
     <div className={`messenger two-column-a ${props.twoColumnATheme}`}>
 
-        <LeftNav />
+      <LeftNav />
 
-        <section>
+      <section>
 
-          <h1>Messenger</h1>
+        <h1>Messenger</h1>
 
-          <div className="messenger-room">
+        <p className="error-message">{feedback}</p>
+
+        <div className="messenger-room">
+          <div className="messenger-connect-disconnect-container">
             <button
               className="messenger-connect-disconnect"
               onClick={props.status === "Connected" ? handleDisconnect : handleConnect}
             >
               {(props.status == "Connected") ? "Disconnect" : "Connect"}
             </button>
-            <label className="messenger-channel-switch" htmlFor="channel-input">Room</label>
-            <input
-              type="text"
-              name="channel-input"
-              defaultValue={props.channel}
-              onKeyUp={(e) => handleChannelChange(e)}
-            />
           </div>
+          <span className="messenger-channel-switch">Room:</span>
+          <span>5067</span>
+          <input
+            className="messenger-channel-input"
+            type="text"
+            name="channel-input"
+            defaultValue={props.channel}
+            onKeyUp={(e) => handleChannelChange(e)}
+          />
+          <button>Enter</button>
+        </div>
 
-          <div className="messenger-main">
+        <div className="messenger-main">
 
-            <div className="messenger-chat">
-              <ul className="messenger-chat-messages" ref={messagesRef}>
-                <li className="messenger-chat-message">
-                  <span className="chat-display-admin">COOK EAT WIN REPEAT</span>
-                </li>
-                {
-                  props.messages && props.messages.map(message => (
-                    message.author === "messengerstatus"
+          <div className="messenger-chat">
+            <ul className="messenger-chat-messages" ref={messagesRef}>
+              <li className="messenger-chat-message">
+                <span className="chat-display-admin">COOK EAT WIN REPEAT</span>
+              </li>
+              {
+                props.messages && props.messages.map(message => (
+                  message.author === "messengerstatus"
+                  ? (
+                    <li className="messenger-chat-message">
+                      <span className="chat-display-admin">{message.text}</span>
+                    </li>
+                  )
+                  : (
+                    props.authname === message.author
                     ? (
                       <li className="messenger-chat-message">
-                        <span className="chat-display-admin">{message.text}</span>
+                        <span className="chat-display-username-self">{message.author}: </span>
+                        {message.text}
                       </li>
                     )
                     : (
-                      props.authname === message.author
-                      ? (
-                        <li className="messenger-chat-message">
-                          <span className="chat-display-username-self">{message.author}: </span>
-                          {message.text}
-                        </li>
-                      )
-                      : (
-                        <li className="messenger-chat-message">
-                          <span className="chat-display-username-other">{message.author}: </span>
-                          {message.text}
-                        </li>
-                      )
+                      <li className="messenger-chat-message">
+                        <span className="chat-display-username-other">{message.author}: </span>
+                        {message.text}
+                      </li>
                     )
-                  ))
-                }
-              </ul>
-              <div className="messenger-chat-input">
-                <input type="text" name="chat-input" onKeyUp={(e) => handleMessageSend(e)} />
-              </div>
+                  )
+                ))
+              }
+            </ul>
+            <div className="messenger-chat-input">
+              <input type="text" name="chat-input" onKeyUp={(e) => handleMessageSend(e)} />
             </div>
-
-            <div className="messenger-people">
-              <div className="messenger-people-tabs">
-                <span
-                  className={`messenger-people-tab ${tab === "Room" && 'chat-nav-current'}`}
-                  onClick={handleUsersInRoomTabClick}
-                >
-                  Room
-                </span>
-                <span
-                  className={`messenger-people-tab ${tab === "Friends" && 'chat-nav-current'}`}
-                  onClick={handleFriendsTabClick}
-                >
-                  Friends
-                </span>
-              </div>
-              <ul className="messenger-users-in-room">
-                {
-                  props.users && props.users.map(user => (
-                    <li className="messenger-user-in-room">
-                      <img src={`https://s3.aws.com/nobscsomething/users/avatars/${user.avatar}`} />
-                      <span>{user.username}</span>
-                    </li>
-                  ))
-                }
-              </ul>
-              <ul className="messenger-friends">
-                {
-                  props.friends && props.friends.map(friend => (
-                    <li className="messenger-friend">
-                      <img src={`https://s3.aws.com/nobscsomething/users/avatars/${friend.avatar}`} />
-                      <span>{friend.username}</span>
-                    </li>
-                  ))
-                }
-              </ul>
-            </div>
-
           </div>
 
-        </section>
+          <div className="messenger-people">
+            <div className="messenger-people-tabs">
+              <button
+                className={(tab === "Room") ? "messenger-people-tab chat-nav-current" : "messenger-people-tab"}
+                onClick={handleUsersInRoomTabClick}
+              >
+                Room
+              </button>
+              <button
+                className={(tab === "Friends") ? "messenger-people-tab chat-nav-current" : "messenger-people-tab"}
+                onClick={handleFriendsTabClick}
+              >
+                Friends
+              </button>
+            </div>
+            {tab === "Room" && (
+              <ul className="messenger-users-in-room">
+                {props.users && props.users.map(user => (
+                  <li className="messenger-user-in-room">
+                    <img src={`https://s3.aws.com/nobscsomething/users/avatars/${user.avatar}`} />
+                    <span>{user.username}</span>
+                  </li>
+                ))}
+              </ul>
+            )}
+            {tab === "Friends" && (
+              <ul className="messenger-friends">
+                {props.friends && props.friends.map(friend => (
+                  <li className="messenger-friend">
+                    <img src={`https://s3.aws.com/nobscsomething/users/avatars/${friend.avatar}`} />
+                    <span>{friend.username}</span>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+        </div>
+
+      </section>
 
     </div>
   );
@@ -166,6 +184,7 @@ const UserMessenger = props => {
 
 const mapStateToProps = state => ({
   authname: state.auth.authname,
+  feedback: state.user.message,
   status: state.messenger.status,
   channel: state.messenger.channel,
   messages: state.messenger.messages,
