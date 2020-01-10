5d113f414da4637abd8162655ded7f6d14b83d5d
T. J. Alferes <tjalferes@gmail.com>
2020-01-10 17:39:06 -0600

    firefox inconsistency

--- src/components/Suggestions/Suggestions.js
+++ src/components/Suggestions/Suggestions.js
@@ -8,51 +8,56 @@ const googleMapsAPIKeyOne = 'AIzaSyCULKDLxoF9O413jjvF5Ot2xXXMdgz0Eag';
 const googleMapsAPIKeyTwo = 'AIzaSyA1caERqL2MD4rv2YmbJ139ToyxgT61v6w';
 
 const Suggestions = props => {
-  const [ latitude, setLatitude ] = useState(null);
-  const [ longitude, setLongitude ] = useState(null);
-  const [ address, setAddress ] = useState(null);
+  const [ latitude, setLatitude ] = useState("");
+  const [ longitude, setLongitude ] = useState("");
+  const [ address, setAddress ] = useState("");
 
   useEffect(() => {
-    const geolocation = navigator.geolocation;
-    geolocation.getCurrentPosition(function(position) {
-      setLatitude(`${position.coords.latitude}`);
-      setLongitude(`${position.coords.longitude}`);
-    });
+    const getLocation = async () => {
+      const geolocation = navigator.geolocation;
+      geolocation.getCurrentPosition(function(position) {
+        setLatitude(`${position.coords.latitude}`);
+        setLongitude(`${position.coords.longitude}`);
+      });
+    };
+    getLocation();
   }, []);
 
   useEffect(() => {
     const getAddress = async () => {
-      if (latitude === null) return;
-      if (longitude === null) return;
-      const res = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${googleMapsAPIKeyTwo}`);
-      setAddress(res.data.results[3].formatted_address);
+      if (latitude === "") return;
+      if (longitude === "") return;
+      const res = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${googleMapsAPIKeyTwo}`
+      );
+      if (res.data) setAddress(res.data.results[3].formatted_address);
     };
     getAddress();
   }, [latitude, longitude]);
 
   return (
     <div className={`suggestions ${props.theme}`}>
-      <span>Stores near you</span>
+      <span className="suggestions__header">Stores near you</span>
       {
-        (latitude !== null && longitude !== null) &&
-        <iframe
-          width="260"
-          height="260"
-          frameBorder="0"
-          style={{border: "0"}}
-          src={`https://www.google.com/maps/embed/v1/search?q=grocery+stores+near+${address}&center=${latitude},${longitude}&zoom=11&key=${googleMapsAPIKeyOne}`}
-          allowFullScreen
-        >
-        </iframe>
+        (address !== "") &&
+        <div id="suggestions__nearby-stores">
+          <iframe
+            id="nearby-stores__iframe"
+            frameBorder="0"
+            style={{border: "0 none"}}
+            src={`https://www.google.com/maps/embed/v1/search?q=grocery+stores+near+${address}&center=${latitude},${longitude}&zoom=11&key=${googleMapsAPIKeyOne}`}
+          >
+          </iframe>
+        </div>
       }
-      <hr />
-      <span>Growers &amp; Ranchers</span>
-      <hr />
-      <span>Stores &amp; Butchers</span>
-      <hr />
-      <span>Popular Now</span>
-      <hr />
-      <span>Suggested for You</span>
+      <hr className="suggestions__line"/>
+      <span className="suggestions__header">Growers &amp; Ranchers</span>
+      <hr className="suggestions__line"/>
+      <span className="suggestions__header">Stores &amp; Butchers</span>
+      <hr className="suggestions__line"/>
+      <span className="suggestions__header">Popular Now</span>
+      <hr className="suggestions__line"/>
+      <span className="suggestions__header">Suggested for You</span>
     </div>
   );
 };
