f267e53cd27d5341d5c8d083daaca9c46131912f
T. J. Alferes <tjalferes@gmail.com>
2020-02-28 20:19:54 -0600

    fancy jest

--- src/components/pages/Food/Cuisine/Cuisine.test.js
+++ src/components/pages/Food/Cuisine/Cuisine.test.js
@@ -1,9 +1,29 @@
 import { shallow } from 'enzyme';
 import React from 'react';
+import routeData from 'react-router';
 
 import { Cuisine } from './Cuisine';
 
+const mockLocation = {
+  pathname: '/welcome',
+  hash: '',
+  search: '',
+  state: ''
+};
+
 describe('Cuisine', () => {
+  beforeEach(() => {
+    /*jest.mock('react-router-dom', () => ({
+      ...jest.requireActual('react-router-dom'), // use actual for all non-hook parts
+      useParams: () => ({
+        companyId: 'company-id1',
+        teamId: 'team-id1',
+      }),
+      useRouteMatch: () => ({ url: '/company/company-id1/team/team-id1' }),
+    }));*/
+    jest.spyOn(routeData, 'useLocation').mockReturnValue(mockLocation);
+  });
+
   it('should load the appropriate cuisine', () => {
     const props = {
       match: {params: {id: "999"}},
