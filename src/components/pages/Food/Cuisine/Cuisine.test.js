c4bc428213f0a855f172b825a49d1d917597d8cc
T. J. Alferes <tjalferes@gmail.com>
2020-02-28 20:48:32 -0600

    avoid mocks

--- src/components/pages/Food/Cuisine/Cuisine.test.js
+++ src/components/pages/Food/Cuisine/Cuisine.test.js
@@ -1,28 +1,75 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import routeData from 'react-router';
+//import routeData from 'react-router';
+//import * as ReactRouterDom from 'react-router-dom';
+
+import { TestingRouter } from '../../../../../test/testUtils';
 
 import { Cuisine } from './Cuisine';
 
-const mockLocation = {
+/*const mockLocation = {
   pathname: '/welcome',
   hash: '',
   search: '',
   state: ''
-};
+};*/
+
+describe('Cuisine Redirects', () => {
+  it('should redirect to /food/cuisines if given no cuisine', () => {
+    const givenProps = {
+      match: {params: {id: undefined}},
+      oneColumnATheme: "light",
+      dataCuisines: [
+        {cuisine_id: 1, cuisine_name: "Chinese"},
+        {cuisine_id: 2, cuisine_name: "Italian"}
+      ]
+    };
+    const container = render(
+      <TestingRouter
+        ComponentWithRedirection={() => <Cuisine {...givenProps} />}
+        RedirectUrl={'/food/cuisines'}
+      />
+    );
+    expect(container[0].children[0].data).toEqual('/food/cuisines');
+  });
+
+  it('should redirect to /food/cuisines if given a non-existent/invalid cuisine', () => {
+    const givenProps = {
+      match: {params: {id: "999"}},
+      oneColumnATheme: "light",
+      dataCuisines: [
+        {cuisine_id: 1, cuisine_name: "Chinese"},
+        {cuisine_id: 2, cuisine_name: "Italian"}
+      ]
+    };
+    const container = render(
+      <TestingRouter
+        ComponentWithRedirection={() => <Cuisine {...givenProps} />}
+        RedirectUrl={'/food/cuisines'}
+      />
+    );
+    expect(container[0].children[0].data).toEqual('/food/cuisines');
+  });
+});
 
 describe('Cuisine', () => {
-  beforeEach(() => {
-    /*jest.mock('react-router-dom', () => ({
-      ...jest.requireActual('react-router-dom'), // use actual for all non-hook parts
-      useParams: () => ({
-        companyId: 'company-id1',
-        teamId: 'team-id1',
-      }),
+  // wrong approach!
+  /*beforeEach(() => {
+    jest.mock('react-router-dom', () => ({
+      ...jest.requireActual('react-router-dom'),
+      useParams: () => ({companyId: 'company-id1', teamId: 'team-id1'}),
       useRouteMatch: () => ({ url: '/company/company-id1/team/team-id1' }),
-    }));*/
+    }));
+
+    jest.mock('react-router-dom', () => ({
+      ...jest.requireActual('react-router-dom'),
+      useHistory: () => ({push: jest.fn()})
+    }));
+
+    jest.spyOn(ReactRouterDom, 'useHistory').returnValue({push: mockHistoryPush});
+
     jest.spyOn(routeData, 'useLocation').mockReturnValue(mockLocation);
-  });
+  });*/
 
   it('should load the appropriate cuisine', () => {
     const props = {
@@ -36,36 +83,6 @@ describe('Cuisine', () => {
 
     const wrapper = shallow(<Cuisine {...props} />);
 
-    expect(wrapper.state("cuisine")).not.toBeNull();  // insufficient?
-  });
-
-  it('should redirect to /food/cuisines if given no cuisine', () => {
-    const props = {
-      match: {params: {id: "999"}},
-      oneColumnATheme: "light",
-      dataCuisines: [
-        {cuisine_id: 1, cuisine_name: "Chinese"},
-        {cuisine_id: 2, cuisine_name: "Italian"}
-      ]
-    };
-
-    const wrapper = shallow(<Cuisine {...props} />);
-
-
-  });
-
-  it('should redirect to /food/cuisines if given a non-existent/invalid cuisine', () => {
-    const props = {
-      match: {params: {id: "999"}},
-      oneColumnATheme: "light",
-      dataCuisines: [
-        {cuisine_id: 1, cuisine_name: "Chinese"},
-        {cuisine_id: 2, cuisine_name: "Italian"}
-      ]
-    };
-
-    const wrapper = shallow(<Cuisine {...props} />);
-
-
+    expect(wrapper.state("cuisine")).not.toBeNull();  // insufficient
   });
 });
