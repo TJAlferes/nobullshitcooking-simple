b11b1334c92e680f2bb4403cd9c3fe60ab5d17fd
T. J. Alferes <tjalferes@gmail.com>
2020-03-02 11:56:10 -0600

    not bad

--- src/components/user/Register/register.test.js
+++ src/components/user/Register/register.test.js
@@ -1,5 +1,6 @@
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 
 import { Register } from './Register';
 
@@ -9,101 +10,152 @@ const authUserVerify = jest.fn();
 let wrapper;
 
 beforeEach(() => {
-  wrapper = shallow(
-    <Register
-      message="Some message."
-      authUserRegister={authUserRegister}
-      authUserVerify={authUserVerify}
-      childProps={{confirmingUser: "false"}}
-    />
+  wrapper = mount(
+    <MemoryRouter>
+      <Register
+        message="Some message."
+        authUserRegister={authUserRegister}
+        authUserVerify={authUserVerify}
+        childProps={{confirmingUser: "false"}}
+      />
+    </MemoryRouter>
   );
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
 });
 
 describe('Register', () => {
   it('should submit user registration info', () => {
-    wrapper.setState({username: "Person"});
-    wrapper.setState({email: "person@place.com"});
-    wrapper.setState({password: "secret"});
-    wrapper.setState({passwordAgain: "secret"});
-    wrapper.find('#create_account_button').simulate('click');
+    wrapper.find('input[name="username"]')
+    .simulate('change', {target: {name: "email", value: "Person"}});
+
+    wrapper.find('input[name="email"]')
+    .simulate('change', {target: {name: "email", value: "person@place.com"}});
+    
+    wrapper.find('input[name="password"]')
+    .simulate('change', {target: {name: "password", value: "secret"}});
+
+    wrapper.find('input[name="passwordAgain"]')
+    .simulate('change', {target: {name: "passwordAgain", value: "secret"}});
+
+    wrapper.find('#create_account_button').at(1).simulate('click');
+
     expect(authUserRegister).toBeCalledTimes(1);
   });
 
   it('should not submit when no username is given', () => {
-    wrapper.setState({email: "person@place.com"});
-    wrapper.setState({password: "secret"});
-    wrapper.setState({passwordAgain: "secret"});
-    wrapper.find('#create_account_button').simulate('click');
+    wrapper.find('input[name="email"]')
+    .simulate('change', {target: {name: "email", value: "person@place.com"}});
+    
+    wrapper.find('input[name="password"]')
+    .simulate('change', {target: {name: "password", value: "secret"}});
+
+    wrapper.find('input[name="passwordAgain"]')
+    .simulate('change', {target: {name: "passwordAgain", value: "secret"}});
+
+    wrapper.find('#create_account_button').at(1).simulate('click');
+
     expect(authUserRegister).toBeCalledTimes(0);
   });
 
   it('should not submit when no email is given', () => {
-    wrapper.setState({username: "Person"});
-    wrapper.setState({password: "secret"});
-    wrapper.setState({passwordAgain: "secret"});
-    wrapper.find('#create_account_button').simulate('click');
+    wrapper.find('input[name="username"]')
+    .simulate('change', {target: {name: "email", value: "Person"}});
+    
+    wrapper.find('input[name="password"]')
+    .simulate('change', {target: {name: "password", value: "secret"}});
+
+    wrapper.find('input[name="passwordAgain"]')
+    .simulate('change', {target: {name: "passwordAgain", value: "secret"}});
+
+    wrapper.find('#create_account_button').at(1).simulate('click');
+
     expect(authUserRegister).toBeCalledTimes(0);
   });
 
   it('should not submit when no password is given', () => {
-    wrapper.setState({username: "Person"});
-    wrapper.setState({email: "person@place.com"});
-    wrapper.find('#create_account_button').simulate('click');
+    wrapper.find('input[name="username"]')
+    .simulate('change', {target: {name: "email", value: "Person"}});
+
+    wrapper.find('input[name="email"]')
+    .simulate('change', {target: {name: "email", value: "person@place.com"}});
+
+    wrapper.find('#create_account_button').at(1).simulate('click');
+
     expect(authUserRegister).toBeCalledTimes(0);
   });
 
   it('should not submit when given passwords are different', () => {
-    wrapper.setState({username: "Person"});
-    wrapper.setState({email: "person@place.com"});
-    wrapper.setState({password: "secret"});
-    wrapper.setState({passwordAgain: "secre"});
-    wrapper.find('#create_account_button').simulate('click');
+    wrapper.find('input[name="username"]')
+    .simulate('change', {target: {name: "email", value: "Person"}});
+
+    wrapper.find('input[name="email"]')
+    .simulate('change', {target: {name: "email", value: "person@place.com"}});
+    
+    wrapper.find('input[name="password"]')
+    .simulate('change', {target: {name: "password", value: "secret"}});
+
+    wrapper.find('input[name="passwordAgain"]')
+    .simulate('change', {target: {name: "passwordAgain", value: "secre"}});
+
+    wrapper.find('#create_account_button').at(1).simulate('click');
+
     expect(authUserRegister).toBeCalledTimes(0);
   });
 
   it('should not submit when username is less than 2 characters', () => {
-    wrapper.setState({username: "P"});
-    wrapper.setState({email: "person@place.com"});
-    wrapper.setState({password: "secret"});
-    wrapper.setState({passwordAgain: "secret"});
-    wrapper.find('#create_account_button').simulate('click');
+    wrapper.find('input[name="username"]')
+    .simulate('change', {target: {name: "email", value: "P"}});
+
+    wrapper.find('input[name="email"]')
+    .simulate('change', {target: {name: "email", value: "person@place.com"}});
+    
+    wrapper.find('input[name="password"]')
+    .simulate('change', {target: {name: "password", value: "secret"}});
+
+    wrapper.find('input[name="passwordAgain"]')
+    .simulate('change', {target: {name: "passwordAgain", value: "secret"}});
+
+    wrapper.find('#create_account_button').at(1).simulate('click');
+
     expect(authUserRegister).toBeCalledTimes(0);
   });
 
   it('should not submit when email is less than 5 characters', () => {
-    wrapper.setState({username: "Person"});
-    wrapper.setState({email: "p@p."});
-    wrapper.setState({password: "secret"});
-    wrapper.setState({passwordAgain: "secret"});
-    wrapper.find('#create_account_button').simulate('click');
+    wrapper.find('input[name="username"]')
+    .simulate('change', {target: {name: "email", value: "Person"}});
+
+    wrapper.find('input[name="email"]')
+    .simulate('change', {target: {name: "email", value: "p@p."}});
+    
+    wrapper.find('input[name="password"]')
+    .simulate('change', {target: {name: "password", value: "secret"}});
+
+    wrapper.find('input[name="passwordAgain"]')
+    .simulate('change', {target: {name: "passwordAgain", value: "secret"}});
+
+    wrapper.find('#create_account_button').at(1).simulate('click');
+
     expect(authUserRegister).toBeCalledTimes(0);
   });
 
   it('should not submit when password is less than 6 characters', () => {
-    wrapper.setState({username: "Person"});
-    wrapper.setState({email: "person@place.com"});
-    wrapper.setState({password: "secre"});
-    wrapper.setState({passwordAgain: "secre"});
-    wrapper.find('#create_account_button').simulate('click');
+    wrapper.find('input[name="username"]')
+    .simulate('change', {target: {name: "email", value: "Person"}});
+
+    wrapper.find('input[name="email"]')
+    .simulate('change', {target: {name: "email", value: "person@place.com"}});
+    
+    wrapper.find('input[name="password"]')
+    .simulate('change', {target: {name: "password", value: "secre"}});
+
+    wrapper.find('input[name="passwordAgain"]')
+    .simulate('change', {target: {name: "passwordAgain", value: "secre"}});
+
+    wrapper.find('#create_account_button').at(1).simulate('click');
+
     expect(authUserRegister).toBeCalledTimes(0);
   });
 });
-
-/*describe('Register', () => {
-  it('can get `isAuthenticated` state', () => {
-    //const wrapper = setup({isAuthenticated});
-    const wrapper = shallow(<Register />).dive();
-    const isAuthenticatedProp = wrapper.instance().props.isAuthenticated;
-    expect(isAuthenticatedProp).toBe(true);
-  });
-  it('can get `message` state', () => {
-    const wrapper = setup({message});
-    const messageProp = wrapper.instance().props.message;
-    expect(messageProp).toBe(true);
-  });
-  test('`authUserRegister` action creator is a function on the props', () => {
-    const wrapper = setup({message});
-    const authUserRegisterProp = wrapper.instance().props.authUserRegister;
-    expect(authUserRegisterProp).toBeInstanceOf(Function);
-  });
-});*/
